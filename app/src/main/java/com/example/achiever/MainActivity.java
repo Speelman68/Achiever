f69125041cf4f91fc782994ade7590390ae2f91e
antoniomartinez <mar20126@byui.edu>
2021-11-05 18:27:33 -0600

    This may be in a new branch!

--- app/src/main/java/com/example/achiever/MainActivity.java
+++ app/src/main/java/com/example/achiever/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("THis is Christian.");
+        System.out.println("This may be in a new branch!");
     }
 }

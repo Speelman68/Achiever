371c27c64aed35c04e24168ce9cb58320760a32d
Brian <brianrallen20@gmail.com>
2021-11-09 15:28:50 -0700

    Added Firebase to the app. There is no functionality it just connects to firebase. This included several changes to the gradle files.

--- app/src/main/java/com/example/achiever/FireBaseLoginActivity.java
+++ app/src/main/java/com/example/achiever/FireBaseLoginActivity.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class FireBaseLoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        System.out.println("THis is Christian.");
+        setContentView(R.layout.activity_firebase_login);
     }
 }

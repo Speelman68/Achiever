1b536febe0dcc9eb9755f7a03230642127f03e9b
Speelmasters <cspeelman68@gmail.com>
2021-11-12 18:03:29 -0700

    Updating the Notification system.

--- app/src/main/java/com/example/achiever/MainActivity.java
+++ app/src/main/java/com/example/achiever/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.achiever;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.achiever.calendar.WeekViewActivity;
-import com.example.achiever.notifications.NotificationUtils;
+import com.example.achiever.notifications.NotificationReceiver;
+
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class MainActivity extends AppCompatActivity{
@@ -18,6 +23,7 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myAlarm();
     }
 
 
@@ -31,11 +37,28 @@ public class MainActivity extends AppCompatActivity{
     {
 
         startActivity(new Intent(this, WeekViewActivity.class));
+    }
 
-        NotificationUtils _notificationUtils = new NotificationUtils(this);
-        long _currentTime = System.currentTimeMillis();
-        long tenSeconds = 1000 * 20;
-        long _triggerReminder = _currentTime + tenSeconds; //triggers a reminder after 10 seconds.
-        _notificationUtils.setReminder(_triggerReminder);
+    public void myAlarm() {
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 17);
+        calendar.set(Calendar.MINUTE, 47);
+        calendar.set(Calendar.SECOND, 0);
+
+        Date currentTime = Calendar.getInstance().getTime();
+
+        if (calendar.getTime().compareTo(new Date()) < 0)
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
+
+        Intent intent = new Intent(getApplicationContext(), NotificationReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        if (alarmManager != null) {
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 10000, pendingIntent);
+            //AlarmManager.INTERVAL_DAY
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/achiever/notifications/NotificationReceiver.java
@@ -0,0 +1,17 @@
+package com.example.achiever.notifications;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NotificationReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        NotificationHelper notificationHelper = new NotificationHelper(context);
+        notificationHelper.createNotification();
+
+    }
+
+}

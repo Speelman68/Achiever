a0898c0fefd4b745f3d566f295a71db79c96bf36
Brian <brianrallen20@gmail.com>
2021-11-16 17:30:12 -0700

    Authentication works with Email and password. Added some error checking on the settings page.

--- app/src/main/java/com/example/achiever/FireBaseLoginActivity.java
+++ app/src/main/java/com/example/achiever/FireBaseLoginActivity.java
@@ -2,114 +2,186 @@ package com.example.achiever;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptionsExtension;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.concurrent.TimeUnit;
 
 public class FireBaseLoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
     private static final String TAG = "EmailPassword";
+    private GoogleApi mGoogleApi;
 
-    String email = "brianrallen20@gmail.com";
-    String password = "12345";
+    private EditText emailString;
+    private EditText passwordString;
+    private Button signInPress;
+    private Button createAccountPress;
+    private SignInButton googleSignInPress;
+    private Button showHideBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase_login);
 
+        passwordString =findViewById(R.id.editTextPassword);
+        emailString = findViewById(R.id.editTextEmail);
+        signInPress = findViewById(R.id.signInButton);
+        createAccountPress = findViewById(R.id.createAccountButton);
+        googleSignInPress = findViewById(R.id.sign_in_button);
+
         mAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleApi = GoogleSignIn.getClient(this, gso);
+
+        signInPress.setOnClickListener(view -> {
+            String email = emailString.getText().toString();
+            String password = passwordString.getText().toString();
+            signIn(email, password);
+        });
+
+        createAccountPress.setOnClickListener(view -> {
+            String email = emailString.getText().toString();
+            String password = passwordString.getText().toString();
+            createAccount(email, password);
+        });
+
+        googleSignInPress.setOnClickListener(view -> {
+          googleSignIn();
+        });
+
     }
 
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
+        GoogleSignInAccount googleAccount = GoogleSignIn.getLastSignedInAccount(this);
+
+        if(currentUser != null || googleAccount != null){
+            signInPress.setText("Sign Out");
+            createAccountPress.setVisibility(View.GONE);
+            googleSignInPress.setVisibility((View.GONE));
             reload();
         }
-    }
-
-    public void newAccountButton(View view)
-    {
-        createAccount(email, password);
+        else
+        {
+            signInPress.setText("Sign In");
+            createAccountPress.setVisibility(View.VISIBLE);
+            googleSignInPress.setVisibility((View.VISIBLE));
+        }
     }
 
     /*Create a new createAccount method that takes in an email address and password
     validates them, and then creates a new user with the createUserWithEmailAndPassword method.*/
     private void createAccount(String email, String password)
     {
-        // [START create_user_with_email]
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(FireBaseLoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
+        if(emailString.getText().length() >= 1 || passwordString.getText().length() >= 1)
+        {
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(FireBaseLoginActivity.this, task -> {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(FireBaseLoginActivity.this, "Created Account Succsefuly", Toast.LENGTH_SHORT).show();
                         }
-                    }
-                });
-        // [END create_user_with_email]
-    }
+                        else
+                        {
+                            Toast.makeText(FireBaseLoginActivity.this, "Could not Create Account", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+        else
+        {
+            Toast.makeText(FireBaseLoginActivity.this, "Email or Password Invalid", Toast.LENGTH_SHORT).show();
+        }
 
-    public void normalSignIn(View view)
-    {
-
-        signIn(email, password);
     }
 
     private void signIn(String email, String password) {
         // [START sign_in_with_email]
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(FireBaseLoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
-                    }
-                });
-        // [END sign_in_with_email]
+        if(emailString.getText().length() >= 1 || passwordString.getText().length() >= 1 || signInPress.getText() == "Sign Out") {
+            if (signInPress.getText() == "Sign In") {
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(FireBaseLoginActivity.this, task -> {
+                            if(task.isSuccessful()) {
+                                Toast.makeText(FireBaseLoginActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+                                signInPress.setText("Sign Out");
+                                createAccountPress.setVisibility(View.GONE);
+                                googleSignInPress.setVisibility((View.GONE));
+                            }else {
+                                Toast.makeText(FireBaseLoginActivity.this, "Unable to Login", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            } else {
+                signOut();
+            }
+        } else {
+            Toast.makeText(FireBaseLoginActivity.this, "Email or Password Invalid", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void googleSignIn()
+    {
+        //Intent intent = mGoogleApi.
+        //startActivityForResult(intent, RC_SIGN_IN);
+        Toast.makeText(FireBaseLoginActivity.this, "Unable to Login", Toast.LENGTH_SHORT).show();
+    }
+
+
+    private void signOut() {
+        mAuth.signOut();
+        Toast.makeText(FireBaseLoginActivity.this, "Logout Success", Toast.LENGTH_SHORT).show();
+        signInPress.setText("Sign In");
+        createAccountPress.setVisibility(View.VISIBLE);
+        googleSignInPress.setVisibility((View.VISIBLE));
+        updateUI(null);
     }
 
     private void sendEmailVerification() {
         // Send verification email
         // [START send_email_verification]
         final FirebaseUser user = mAuth.getCurrentUser();
+        assert user != null;
         user.sendEmailVerification()
-                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        // Email sent
-                    }
+                .addOnCompleteListener(this, task -> {
+                    // Email sent
                 });
         // [END send_email_verification]
     }

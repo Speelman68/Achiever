84faef1dea7847ca9caa93ecab5a041f265f5e8d
bamph1911 <pat14012@byui.edu>
2021-12-07 16:21:30 -0700

    Devin adding the TimerScreen and activity.xml. Current issue is that app keeps crashing during onclick in the EventAdapter. Intent seems to not be working as intended.

--- app/src/main/java/com/example/achiever/calendar/EventAdapter.java
+++ app/src/main/java/com/example/achiever/calendar/EventAdapter.java
@@ -10,12 +10,14 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 
 import com.example.achiever.R;
 import com.example.achiever.goals.DesignHabit;
+import com.example.achiever.goals.DisplayHabit;
 import com.example.achiever.goals.Habit;
 
 import java.text.SimpleDateFormat;
@@ -24,13 +26,20 @@ import java.time.format.TextStyle;
 import java.util.List;
 import java.util.Locale;
 
+
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class EventAdapter extends ArrayAdapter<Habit> {
     EventAdapter eventAdapter;
+    private Context context;
+    Intent intent;
+
+
     public EventAdapter(@NonNull Context context, List<Habit> events)
     {
+
         super(context, 0, events);
     }
+
 
     @NonNull
     @Override
@@ -47,13 +56,19 @@ public class EventAdapter extends ArrayAdapter<Habit> {
         boolean isCurrentDay = (dayS.equals(current));
 
         TextView eventCellTV = convertView.findViewById(R.id.eventCellTV);
-        Button btn=(Button) convertView.findViewById(R.id.start);
 
+        Button btn=(Button) convertView.findViewById(R.id.start);
         btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 habit.completedDays.put(dayS, true);
                 eventAdapter.notifyDataSetChanged();
+
+//                Intent timerIntent = new Intent(context, TimerScreen.class);
+//                v.getContext().startActivity(timerIntent);
+                Intent timerIntent = new Intent(v.getContext(), TimerScreen.class);
+                v.getContext().startActivity(timerIntent);
+
             }
         });
 

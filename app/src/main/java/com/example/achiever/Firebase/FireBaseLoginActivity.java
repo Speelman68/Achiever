22c09b6c2ce06ae85019e04fbb4b7a475a9c3eeb
Brian <brianrallen20@gmail.com>
2021-12-11 16:36:24 -0700

    Made it so that when you log in it pulls down your habits from firestore.
    Errors: It does not display the name of the habit. It only pulls down the habits when you log in.

--- app/src/main/java/com/example/achiever/Firebase/FireBaseLoginActivity.java
+++ app/src/main/java/com/example/achiever/Firebase/FireBaseLoginActivity.java
@@ -5,7 +5,6 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
